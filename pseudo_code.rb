0c21e567579e3e18107a6914ad0ab8c7803aa015
Spencer Limbach <sjlimbach@gmail.com>
2020-12-30 19:49:27 +0000

    Lesson 2

---
+++ pseudo_code.rb
@@ -0,0 +1,21 @@
+# 1. a method that returns the sum of two integers
+
+START 
+
+GET integers from user 
+  - make sure they are valid, otherwise ask again 
+SET integers = x, y 
+  - perform function x + y 
+PRINT result 
+
+END 
+
+# 2. a method that takes an array of strings, and returns a string that is all those strings concatenated together
+
+START 
+
+SET 
+
+# 3. a method that takes an array of integers, and returns a new array with every other element
+
+
